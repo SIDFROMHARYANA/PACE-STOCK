cb7eb43cfd3a91b0e6a8c758f9f07f9b6162944b
SIDDHARTH.M <110590347+SIDFROMHARYANA@users.noreply.github.com>
2023-02-19 00:56:46 +0530

    Add files via upload

---
+++ BLOG.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Artikel from "../components/Artikel";
+import FetchData from "../components/FetchData";
+
+ const BLOG = () =>
+ {
+    return (
+     <div>
+        <Artikel />
+        <FetchData />
+    </div>
+    );
+ };
+
+ export default BLOG;
---
+++ Categories.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+
+ const Categories = ({ cat }) =>
+ {
+    return 
+        <div>{cat}</div>;
+    
+ };
+
+ export default Categories;
